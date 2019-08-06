2bc16dd80f175d3b2c3392ad1b9eea78a89c6608
borjarobles <borja.robles@gmail.com>
2019-08-06 13:10:56 +0200

    stuff

--- cypress/integration/examples/window.spec.js
+++ cypress/integration/examples/window.spec.js
@@ -7,7 +7,7 @@ context('Window', () => {
 
   it('cy.window() - get the global window object', () => {
     // https://on.cypress.io/window
-             cy.window().should('have.property', 'top')
+          cy.window().should('have.property', 'top')
   })
 
   it('cy.document() - get the document object', () => {
