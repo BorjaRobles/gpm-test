9b256f65996c347c903c5ac559636b251b1b3e72
borjarobles <borja.robles@gmail.com>
2019-08-10 23:54:56 +0200

    reports

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,7 +2,6 @@
 
 declare namespace Cypress {
     interface Chainable<Subject> {
-
         getById(id: string): Chainable<any>
         getByPartialId(id: string): Chainable<any>
         waitForXHR(method: string, url: string): Chainable<any>
